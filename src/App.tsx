ec5d7251caf625be45acb7180368aabf5dfba170
Suhel <suhel7822@gmail.com>
2024-01-28 12:27:16 +0530

    implement routing, change navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import Home from "./components/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
 import Navbar from "./components/Navbar";
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/gameplay",
+    element: <div>Gameplay will come here</div>,
+  },
+]);
 
 const App = () => {
   return (
     <div className="h-screen">
       <div className="app-background"></div>
       <Navbar />
-      <Home />
+      <RouterProvider router={Router} />
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,23 @@ import { VscGithubInverted } from "react-icons/vsc";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center items-center pt-6 fixed top-0 w-full">
-      <div className="flex justify-center items-center gap-6 width-[240px]">
-        <VscGithubInverted className="text-white text-[32px]" />
+    <nav className="text-green-300 flex justify-center items-center pt-6 fixed top-0 w-full gap-10">
+      <div className="flex justify-center items-center gap-6">
+        <a href="https://github.com/Suhelkhan12" target="_blank">
+          <VscGithubInverted className=" text-[32px]" />
+        </a>
+      </div>
+      <div>
+        <p>
+          Made with ❤️ by{" "}
+          <a
+            href="#https://www.linkedin.com/in/suhel-khan-925aa222a/"
+            target="_blank"
+            className="text-blue-400 underline font-medium"
+          >
+            Suhel
+          </a>
+        </p>
       </div>
     </nav>
   );
