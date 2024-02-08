0927836353f8aa02a1372779e963e3ae531a10fc
Suhel <suhel7822@gmail.com>
2024-02-08 22:37:24 +0530

    add all edge cases for the wordle alogrithm

--- src/pages/Gameplay.tsx
+++ src/pages/Gameplay.tsx
@@ -1,6 +1,8 @@
 import WordRow from "../components/UI/WordRow";
+import { checkValidityOfGuess } from "../utils/word-util";
 
 const Gameplay = () => {
+  console.log(checkValidityOfGuess("allol", "smelt"));
   return (
     <main className="h-screen flex justify-center pt-10  text-white">
       <div className="flex flex-col gap-2">
--- src/utils/word-util.ts
+++ src/utils/word-util.ts
@@ -18,6 +18,11 @@ export async function getRandomWord(){
 // the guess which user entered I have to compare it letter by letter with the original
 // letter and for every letter I'm going to return a letter state that is why this function
 // has LetterStates[] return type. Just think about the rules of wordle..
+
+/**
+ * well it is not quite good implementation because it will not work for more than one letter present in the user input
+ * allol and smelt :- first l in allol will give match for smelt, simillarly it will give yess for second and third l because l in smelt will always exist
+ */
 export function checkValidityOfGuess(guess:string, generatedWord:string): LetterStates[]{
     console.log({guess,generatedWord})
     const guessArray = guess.split('');
@@ -26,18 +31,50 @@ export function checkValidityOfGuess(guess:string, generatedWord:string): Letter
     // setting type of result
     const result: LetterStates[] = [];
 
-    generatedWordArray.forEach((currGeneratedWordLetter, i) => {
-        const currGuessLetter = guessArray[i];
-        
-        if (currGuessLetter === currGeneratedWordLetter) {
+    if(guessArray.length !== generatedWordArray.length){
+        return result;
+    }
+
+    // basicallya map is created here
+    const answerLetterCount: Record<string, number> = {};
+
+    guessArray.forEach((letter , i)=>{
+        const currAnswerLetter = generatedWordArray[i];
+
+        answerLetterCount[currAnswerLetter] = answerLetterCount[currAnswerLetter] ? answerLetterCount[currAnswerLetter]+1 : 1;
+
+        if(currAnswerLetter === letter){
             result.push(LetterStates.Match);
-        } else if (guessArray.includes(currGeneratedWordLetter)) {
-            result.push(LetterStates.Present);
-        } else {
-            result.push(LetterStates.Miss);
+        }else if(generatedWordArray.includes(letter)){
+            result.push(LetterStates.Present)
+        }else{
+            result.push(LetterStates.Miss)
         }
     });
-    
 
+    result.forEach((currResult, resultIndex)=>{
+        // if no letter is present there
+        if(currResult !== LetterStates.Present){
+            return;
+        }
+
+        const guessLetter = guessArray[resultIndex];
+
+        generatedWordArray.forEach((currAnsLetter , answerIndex)=>{
+            if(currAnsLetter !== guessLetter){
+                return;
+            }
+            
+            if(result[answerIndex] === LetterStates.Match){
+                result[resultIndex] = LetterStates.Miss
+            }
+
+            if(answerLetterCount[guessLetter]<= 0){
+                result[resultIndex] = LetterStates.Miss
+            }
+        });
+
+        answerLetterCount[guessLetter]--;
+    })
     return result;
 }
