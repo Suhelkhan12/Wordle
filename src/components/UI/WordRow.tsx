e5f847034e566c54ea97ebe4ebb47b0d96bbda8c
Suhel <suhel7822@gmail.com>
2024-04-22 23:40:59 +0530

    changed ui or letter box and wordrow

--- src/components/UI/WordRow.tsx
+++ src/components/UI/WordRow.tsx
@@ -1,22 +1,48 @@
-import LetterBox from "./LetterBox";
+import { LetterStates } from "../../types";
 import { checkValidityOfGuess } from "../../utils/word-util";
+
 type WordRowPropType = {
   letter: string;
+};
+
+const LETTER_LENGTH = 5;
+
+type LetterBoxProp = {
+  letter: string;
+  letterState?: LetterStates;
+};
+
+// for styling letter according to letterState basesd ongues
+const letterStateStyles = {
+  [LetterStates.Miss]: "bg-red-500 border-red-500",
+  [LetterStates.Present]: "bg-yellow-500 border-yellow-500",
+  [LetterStates.Match]: "bg-green-500 border-green-500",
+};
+
+const LetterBox = ({ letter, letterState }: LetterBoxProp) => {
+  // getting the style for individual letter bos
+  const letterBoxStyle =
+    letterState === undefined ? "" : letterStateStyles[letterState];
+
+  return (
+    <span
+      className={` ${letterBoxStyle} flex justify-center items-center border-[2px] border-gray-400 sm:text-[2rem] text-[28px] uppercase font-bold sm:w-14 sm:h-14 w-12 h-12  `}
+    >
+      {letter}
+    </span>
+  );
 };
 
 const WordRow = ({ letter }: WordRowPropType) => {
   const guessState = checkValidityOfGuess(letter);
   // we did this so that we can conver our string letter into array and we can easity map over it
-  const letterArray = letter.split("");
+  const letterRemaining = LETTER_LENGTH - letter.length;
+  const letterArray = letter.split("").concat(Array(letterRemaining).fill(""));
   return (
     <div className="grid grid-cols-5 gap-2 ">
       {letterArray.length > 0 &&
         letterArray.map((letter, ind) => (
-          <LetterBox
-            key={Math.random()}
-            letter={letter}
-            letterState={guessState[ind]}
-          />
+          <LetterBox key={ind} letter={letter} letterState={guessState[ind]} />
         ))}
     </div>
   );
